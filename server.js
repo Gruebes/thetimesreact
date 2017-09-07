27ae9956b1ca7d0c5744f545570bb6053dea54c7
TannerGruebele <tanner@saambaa.com>
2017-09-06 22:38:58 -0700

    Building for production

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use("/", routes);
 
 var db = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 
+//mongodb://heroku_c6lggq5r:4nlrkoc0rughagu6iq8fn95ldq@ds129434.mlab.com:29434/heroku_c6lggq5r
+
 // Connect mongoose to our database
 mongoose.connect(db, function(error) {
   // Log any errors connecting with mongoose
